b9b4611e93461609b0a70d4bcdc96e36600ac019
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2026-09-05 04:51:37 +0530

    chore: remove TopBar component from app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,3 @@
-import { TopBar } from "@/components/layout/top-bar";
 import { BottomNav } from "@/components/layout/bottom-nav";
 import { UserProvider } from "@/lib/store/user-store";
 
@@ -10,7 +9,6 @@ export default function AppLayout({
   return (
     <UserProvider>
       <div className="flex flex-col min-h-screen bg-[#f8faf9]">
-        <TopBar />
         <main className="flex-1 pb-20 w-full max-w-md mx-auto relative">{children}</main>
         <BottomNav />
       </div>
